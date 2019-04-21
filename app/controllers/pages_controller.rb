d3b45abe0a3caaa554818c2bcbac3f55f1292ad6
bknGC <bkngg@yandex.ru>
2019-04-21 21:08:21 +0300

    upd 0.4 - Добавлен функционал для создания и просмотра статей.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def about
-    
+    @text_about_head = 'Привет! "Wetice" - это библиотека самой полезной и интересной информации для изучения чего-то нового!'
+    @text_about_body = 'Мы находим самый новый и качественный контент для комфортного обучения!'
+    @text_about_body1 = 'Развивайтесь и делайте этот мир лучше!'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'articles#index'
 
   get 'about' => 'pages#about'
+
+  resources :articles
+
 end
